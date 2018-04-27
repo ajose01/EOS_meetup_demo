2a2ab69cc18d1a14f4397e880086e71a98cc5d2b
Angel Jose <ajose01@gmail.com>
2018-04-27 13:57:04 -0700

    Refactoring as we learn

--- src/index.js
+++ src/index.js
@@ -1,42 +1,36 @@
 import Eos from 'eosjs'
+let divId = document.getElementById("Title")
+let scatter;
 
 document.addEventListener('scatterLoaded', scatterExtension => {
-    // Scatter will now be available from the window scope.
-    // At this stage the connection to Scatter from the application is·
-    // already encrypted.·
-    const scatter = window.scatter;
-    console.log(scatter, "scatter")
-    // It is good practice to take this off the window once you have·
-    // a reference to it.
-    window.scatter = null;
-    const eosjs = Eos.Localnet({httpEndpoint:'http://159.65.161.242:8888'})
-    window.eosjs = eosjs
+  divId.innerHTML = "SCATTER IS HERE.... Maybe we do have EOS Stuff" 
 
-    // If you want to require a specific version of Scatter
-    scatter.requireVersion(3.0);
+  scatter = window.scatter;
+  document.getElementById("secretButton").addEventListener("click", function(){
+    getIdentity(scatter) 
+  })
+  console.log(scatter, "scatter")
+  // It is good practice to take this off the window once you have·
+  // a reference to it.
+  window.scatter = null
 
-    // Set up the network and options you want to use eosjs and Scatter with.·
-    const network = { host:"159.65.161.242", port:8888 }
-    console.log(network, "network")
-    const eosOptions = {}
+  const eosjs = Eos.Localnet({httpEndpoint:'http://159.65.161.242:8888'})
+  window.eosjs = eosjs
 
-    //const tempEos = Eos.Localnet({httpEndpoint: 'http://169.99.104.110:888'})
-    const tempEos = Eos.Localnet
-    console.log(tempEos, "tempEos")
+  // If you want to require a specific version of Scatter
+  scatter.requireVersion(3.0);
 
-    // Get a reference to an 'Eos.LocalNet' with a Scatter signature provider.
-    const eos = scatter.eos( Eos.Localnet, network, eosOptions )
-    window.eos = eos
-    console.log(eos, "EOS")
+  // Set up the network and options you want to use eosjs and Scatter with.·
+  const network = { host:"159.65.161.242", port:8888 }
+  console.log(network, "network")
+  const eosOptions = {}
+
+  // Get a reference to an 'Eos.LocalNet' with a Scatter signature provider.
+  const eos = scatter.eos( Eos.Localnet, network, eosOptions )
+  window.eos = eos
+  console.log(eos, "EOS")
 
   console.log(scatter.identity, "id1")
-
-  scatter.getIdentity(['account']).then(identity => {
-    console.log(identity, "identity")
-    window.identity = identity
-  }).catch(error => {
-    console.log(error, "identityError")
-  });
 
   console.log(scatter.identity, "id2")
   let name = scatter.identity.account.name
@@ -49,15 +43,34 @@ document.addEventListener('scatterLoaded', scatterExtension => {
 
   eosjs.getAccount(name).then(acc => {
     console.log(acc)
-      window.eosjsaccount = acc
-  }).catch(error => {
-    console.log(error)
-  })
-
-  eosjs.contract('eosio.token').then(contract => {
-    window.contract = contract
-      console.log(contract)
+    window.eosjsaccount = acc
   }).catch(error => {
     console.log(error)
   })
 })
+
+let getEosBalance = (account) => {
+  eosjs.getTableRows({
+    scope: "aihgh",
+    code: "eosio.token",
+    table: "accounts",
+    json: true})
+    .then((x) => {
+      console.log(x)
+    })
+    .catch((e) => 
+      console.log(e)
+    )
+}
+
+let getIdentity = (_scatter) => {
+  _scatter.getIdentity(['account']).then(identity => {
+    window.identity = identity
+    divId.innerHTML = "ALL the EOS goodies are yours!"
+  }).catch(error => {
+    console.log(error, "identityError")
+    divId.innerHTML = "Ok... soooo close... no EOS for you"
+  });
+}
+
+window.getEosBalance = getEosBalance
