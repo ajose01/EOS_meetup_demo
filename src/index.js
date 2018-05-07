6f59c722b54344fcae72decda97df03e1a690f1f
Angel Jose <ajose01@gmail.com>
2018-05-07 09:45:42 -0700

    Match demo to blog post

--- src/index.js
+++ src/index.js
@@ -1,76 +1,46 @@
 import Eos from 'eosjs'
-let divId = document.getElementById("Title")
-let scatter;
+// Let's make it easy to grab these DOM elements.
+let title = document.getElementById("Title")
+let vid = document.getElementById("vid")
+let button = document.getElementById("secretButton")
 
 document.addEventListener('scatterLoaded', scatterExtension => {
-  divId.innerHTML = "SCATTER IS HERE.... Maybe we do have EOS Stuff" 
+  const scatter = window.scatter
+  // uncomment following in actual projects
+  // window.scatter = null
 
-  scatter = window.scatter;
-  document.getElementById("secretButton").addEventListener("click", function(){
-    getIdentity(scatter) 
+  // If the scatterLoaded event is detected, we change the messaging as follows
+  title.innerHTML = "Scatter found! Maybe we do have EOS stuff"
+  vid.src = "https://media.giphy.com/media/DfdbTJZx6Yjra/giphy.mp4"
+  button.removeAttribute("hidden")
+  button.addEventListener("click", function(){
+    getIdentity()
   })
-  console.log(scatter, "scatter")
-  // It is good practice to take this off the window once you have·
-  // a reference to it.
-  window.scatter = null
 
-  const eosjs = Eos.Localnet({httpEndpoint:'http://159.65.161.242:8888'})
-  window.eosjs = eosjs
+  let currentIdentity // We'll hold the identity here
 
-  // If you want to require a specific version of Scatter
-  scatter.requireVersion(3.0);
-
-  // Set up the network and options you want to use eosjs and Scatter with.·
   const network = { host:"159.65.161.242", port:8888 }
-  console.log(network, "network")
   const eosOptions = {}
 
-  // Get a reference to an 'Eos.LocalNet' with a Scatter signature provider.
   const eos = scatter.eos( Eos.Localnet, network, eosOptions )
-  window.eos = eos
-  console.log(eos, "EOS")
 
-  console.log(scatter.identity, "id1")
-
-  console.log(scatter.identity, "id2")
-  let name = scatter.identity.account.name
-  eos.getAccount(name).then(acc => {
-    console.log(acc)
-    window.account = acc
-  }).catch(error => {
-    console.log(error)
-  })
-
-  eosjs.getAccount(name).then(acc => {
-    console.log(acc)
-    window.eosjsaccount = acc
-  }).catch(error => {
-    console.log(error)
-  })
-})
-
-let getEosBalance = (account) => {
-  eosjs.getTableRows({
-    scope: "aihgh",
-    code: "eosio.token",
-    table: "accounts",
-    json: true})
-    .then((x) => {
-      console.log(x)
+  let getIdentity = () => {
+    scatter.getIdentity(['account']).then(identity => {
+      console.log(identity, "identityFound")
+      currentIdentity = identity
+      // do not assign to window in actual projects... only for learning.
+      window.identity = identity
+      // User "Accept" action
+      title.innerHTML = "Welcome to all the EOS goodies " + identity.name
+      vid.src = "https://media.giphy.com/media/lxcz7ntpCKJfq/giphy.mp4"
+      button.setAttribute("hidden", "")
+    }).catch(error => {
+      console.log(error, "identityCrisis!")
+      currentIdentity = "Identity Crisis!"
+      // User "Deny" action
+      title.innerHTML = "Oh so close... maybe next time"
+      vid.src = "https://media.giphy.com/media/2sfHfGhM3zKUzZ5xzh/giphy.mp4"
+      button.setAttribute("hidden", "")
     })
-    .catch((e) => 
-      console.log(e)
-    )
-}
-
-let getIdentity = (_scatter) => {
-  _scatter.getIdentity(['account']).then(identity => {
-    window.identity = identity
-    divId.innerHTML = "ALL the EOS goodies are yours!"
-  }).catch(error => {
-    console.log(error, "identityError")
-    divId.innerHTML = "Ok... soooo close... no EOS for you"
-  });
-}
-
-window.getEosBalance = getEosBalance
+  }
+})
